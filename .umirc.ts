9cbbbe69eef7288ff75b6ece0443f4be0d641046
mizhon <mizhon@live.cn>
2021-01-04 14:14:46 +0800

    add codes

--- .umirc.ts
+++ .umirc.ts
@@ -4,16 +4,8 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  // routes: [
-  //   { path: '/', component: '@/pages/index' },
-  //   {
-  //     path: '/dashboard',
-  //     component: '@/pages/dashboard/index',
-  //   },
-  // ],
-  
-   /**
+  /**
    * 快速刷新
    */
-  fastRefresh: {}
+  fastRefresh: {},
 });
